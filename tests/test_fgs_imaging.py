ba5ae348a900911e40e1c3f38bf38f37ad7e97e7
Johannes Sahlmann <jsahlmann@stsci.edu>
2018-07-17 11:05:00 -0400

    test_nircam_imaging and test_niriss_imaging functional.

--- tests/test_fgs_imaging.py
+++ tests/test_fgs_imaging.py
@@ -1,9 +1,21 @@
+"""System test of mirage/FGS that creates simulations based on a .yaml file.
+
+Authors
+-------
+    - Johannes Sahlmann
+
+Use
+---
+    >>> pytest -s test_fgs_imaging.py
+
+
+"""
 
 import os
 
 from mirage.scripts import imaging_simulator as im
 
-os.environ['MIRAGE_DATA'] = ''
+# os.environ['MIRAGE_DATA'] = ''
 os.environ['TEST_DATA'] = os.path.join(os.path.dirname(__file__), 'test_data/FGS')
 
 def test_fgs_imaging():
