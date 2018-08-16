750e549872779331a0cc980a9a6ce20854f7e66b
Marshall Perrin <mperrin@stsci.edu>
2018-08-16 12:49:57 -0400

    add Sphinx docs framework

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. FALCO documentation master file, created by
+   sphinx-quickstart on Thu Jun 21 20:18:02 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+MIRAGE: Multi-Instrument RAmp GEnerator
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   install.rst
+   quickstart.ipynb
+   api.rst
+
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -7,6 +7,44 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
+# This next part allows you to build sphinx docs from the package
+# main directory with 'python setup.py build_docs'
+
+try:
+    from sphinx.cmd.build import build_main
+    from sphinx.setup_command import BuildDoc
+
+    class BuildSphinx(BuildDoc):
+        """Build Sphinx documentation after compiling C source files"""
+
+        description = 'Build Sphinx documentation'
+
+        def initialize_options(self):
+            BuildDoc.initialize_options(self)
+
+        def finalize_options(self):
+            BuildDoc.finalize_options(self)
+
+        def run(self):
+            build_cmd = self.reinitialize_command('build_ext')
+            build_cmd.inplace = 1
+            self.run_command('build_ext')
+            build_main(['-b', 'html', './docs', './docs/_build/html'])
+
+except ImportError:
+    class BuildSphinx(Command):
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            print('!\n! Sphinx is not installed!\n!', file=sys.stderr)
+            exit(1)
+
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
@@ -74,7 +112,7 @@ setup(
     ],
     include_package_data=True,
     cmdclass={
-        'test': PyTest
-        # 'build_sphinx': BuildSphinx
+        'test': PyTest,
+        'build_docs': BuildSphinx
     },
     )
