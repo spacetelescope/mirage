f58636cf746fd3cc88f8d1466ae2e1b00106319a
Johannes Sahlmann <jsahlmann@stsci.edu>
2018-09-28 15:26:36 -0400

    initial changes to make it work for NIRISS

--- mirage/yaml/write_observationlist.py
+++ mirage/yaml/write_observationlist.py
@@ -4,6 +4,7 @@ output files
 Authors
 -------
     - Lauren Chambers
+    - Johannes Sahlmann
 
 Use
 ---
@@ -20,25 +21,31 @@ import numpy as np
 from ..apt import read_apt_xml
 
 
-def write_yaml(xml_file, pointing_file, yaml_file, ps_cat_sw=None, ps_cat_lw=None):
-    # Define "default" values (probably should be changed eventually)
-    date = '2019-07-04'
-    PAV3 = '0.'
-    GalaxyCatalog = 'None'
-    ExtendedCatalog = 'None'
-    ExtendedScale = '1.0'
-    ExtendedCenter = '1024,1024'
-    MovingTargetList = 'None'
-    MovingTargetSersic = 'None'
-    MovingTargetExtended = 'None'
-    MovingTargetConvolveExtended = 'True'
-    MovingTargetToTrack = 'None'
-    BackgroundRate_sw = '0.5'
-    BackgroundRate_lw = '1.2'
+def write_yaml(xml_file, pointing_file, yaml_file, catalog_files, ps_cat_sw=None, ps_cat_lw=None):
+    """Write observation list file (required miareg input) on the basis of APT files.
+
+    Parameters
+    ----------
+    xml_file : str
+        path to APT .xml file
+    pointing_file
+    yaml_file
+    catalog_files : str or list(str)
+        path to file(s) that contain the catalog of sources.
+    ps_cat_sw
+    ps_cat_lw
+
+    Returns
+    -------
+
+    """
 
     # Read in observations from XML file
     with open(xml_file) as f:
         tree = etree.parse(f)
+
+    if type(catalog_files) is str:
+        catalog_files = [catalog_files]
 
     apt = '{http://www.stsci.edu/JWST/APT}'
     observation_data = tree.find(apt + 'DataRequests')
@@ -48,10 +55,13 @@ def write_yaml(xml_file, pointing_file, yaml_file, ps_cat_sw=None, ps_cat_lw=Non
     observations = []
     i_observations = []
     obs_names = []
+    instruments = []
     for i_obs, o in enumerate(obs_results):
-        if o.find(apt + 'Instrument').text in ['NIRCAM', 'WFSC']:
+        instrument = o.find(apt + 'Instrument').text
+        if instrument in ['NIRCAM', 'WFSC', 'NIRISS', 'FGS']:
             observations.append(o)
             i_observations.append(i_obs)
+            instruments.append(instrument)
 
             # Find observation name, and modify to exclude parantheticals
             label = o.find(apt + 'Label').text
@@ -65,26 +75,84 @@ def write_yaml(xml_file, pointing_file, yaml_file, ps_cat_sw=None, ps_cat_lw=Non
     readxml_obj = read_apt_xml.ReadAPTXML()
     xml_table = readxml_obj.read_xml(xml_file)
 
-    sw_filters = {}
-    lw_filters = {}
-    sw_filters_all = np.array(xml_table['ShortFilter'])
-    lw_filters_all = np.array(xml_table['LongFilter'])
-    tile_nums = xml_table['TileNumber']
-    observation_ids = xml_table['ObservationID']
+    # array of unique instrument names
+    used_instruments =  np.unique(instruments)
 
-    for i_obs_all in set(observation_ids):
-        # i_obs_all = int(i_obs_all)
-        current_obs_indices = [i == i_obs_all for i in observation_ids]
-        if len(set(np.array(sw_filters_all)[current_obs_indices])) > 1:
-            print('Note: Multiple filters in observation {}'.format(i_obs_all))
-            # At some point could use the tile_nums to fix this
-        sw_filters[i_obs_all] = sw_filters_all[current_obs_indices]
-        lw_filters[i_obs_all] = lw_filters_all[current_obs_indices]
+    if len(used_instruments) > 1:
+        # Several instruments used within one APT file, e.g. parallels
+        raise NotImplementedError
+    else:
+        if used_instruments[0] in ['NIRCAM', 'WFSC']:
+            # Define "default" values (probably should be changed eventually)
+            date = '2019-07-04'
+            PAV3 = '0.'
+            GalaxyCatalog = 'None'
+            ExtendedCatalog = 'None'
+            ExtendedScale = '1.0'
+            ExtendedCenter = '1024,1024'
+            MovingTargetList = 'None'
+            MovingTargetSersic = 'None'
+            MovingTargetExtended = 'None'
+            MovingTargetConvolveExtended = 'True'
+            MovingTargetToTrack = 'None'
+            BackgroundRate_sw = '0.5'
+            BackgroundRate_lw = '1.2'
 
-    # Check that all parameters have the right length
-    all_param_lengths = [len(ps_cat_sw), len(ps_cat_lw), len(sw_filters),
-                         len(lw_filters), len(observations), len(i_observations),
-                         len(obs_names)]
+            sw_filters = {}
+            lw_filters = {}
+            sw_filters_all = np.array(xml_table['ShortFilter'])
+            lw_filters_all = np.array(xml_table['LongFilter'])
+            tile_nums = xml_table['TileNumber']
+            observation_ids = xml_table['ObservationID']
+
+            for i_obs_all in set(observation_ids):
+                # i_obs_all = int(i_obs_all)
+                current_obs_indices = [i == i_obs_all for i in observation_ids]
+                if len(set(np.array(sw_filters_all)[current_obs_indices])) > 1:
+                    print('Note: Multiple filters in observation {}'.format(i_obs_all))
+                    # At some point could use the tile_nums to fix this
+                sw_filters[i_obs_all] = sw_filters_all[current_obs_indices]
+                lw_filters[i_obs_all] = lw_filters_all[current_obs_indices]
+
+            # Check that all parameters have the right length
+            all_param_lengths = [len(ps_cat_sw), len(ps_cat_lw), len(sw_filters),
+                                 len(lw_filters), len(observations), len(i_observations),
+                                 len(obs_names)]
+
+        elif used_instruments[0] in ['NIRISS']:
+            # Define "default" values (probably should be changed eventually)
+            date = '2019-07-04'
+            PAV3 = '0.'
+            GalaxyCatalog = 'None'
+            ExtendedCatalog = 'None'
+            ExtendedScale = '1.0'
+            ExtendedCenter = '1024,1024'
+            MovingTargetList = 'None'
+            MovingTargetSersic = 'None'
+            MovingTargetExtended = 'None'
+            MovingTargetConvolveExtended = 'True'
+            MovingTargetToTrack = 'None'
+            BackgroundRate = '0.5'
+
+            filters = {}
+            filters_all = np.array(xml_table['PupilWheel'])
+            # lw_filters_all = np.array(xml_table['LongFilter'])
+            tile_nums = xml_table['TileNumber']
+            observation_ids = xml_table['ObservationID']
+
+            for i_obs_all in set(observation_ids):
+                # i_obs_all = int(i_obs_all)
+                current_obs_indices = [i == i_obs_all for i in observation_ids]
+                if len(set(np.array(filters_all)[current_obs_indices])) > 1:
+                    print('Note: Multiple filters in observation {}'.format(i_obs_all))
+                    # At some point could use the tile_nums to fix this
+                filters[i_obs_all] = filters_all[current_obs_indices]
+                # lw_filters[i_obs_all] = lw_filters_all[current_obs_indices]
+
+            # Check that all parameters have the right length
+            all_param_lengths = [len(catalog_files), len(filters), len(observations),
+                                 len(i_observations), len(obs_names)]
+
     if len(set(all_param_lengths)) > 1:
         print(all_param_lengths)
         raise ValueError('Not all provided parameters have compatible '
@@ -93,46 +161,77 @@ def write_yaml(xml_file, pointing_file, yaml_file, ps_cat_sw=None, ps_cat_lw=Non
     write = ["# Observation list created by write_observationlist.py  in\n",
              "# mirage/yaml. Note: all values except filters and\n",
              "# observation names are default.\n\n"]
-    for i_obs in range(num_obs):
-        obs_number = i_observations[i_obs] + 1
-        write += [
-            "Observation{}:\n".format(obs_number),
-            "  Name: '{}'\n".format(obs_names[i_obs]),
-            "  Date: {}\n".format(date),
-            "  PAV3: {}\n".format(PAV3),
-        ]
 
-        for i_filt, (sw_filt, lw_filt) in enumerate(zip(sw_filters[obs_number],
-                                                        lw_filters[obs_number])):
+    if used_instruments[0] in ['NIRCAM', 'WFSC']:
+        for i_obs in range(num_obs):
+            obs_number = i_observations[i_obs] + 1
             write += [
-                "  FilterConfig{}:\n".format(i_filt + 1),
-                "    SW:\n",
-                "      Filter: {}\n".format(sw_filt),
-                "      PointSourceCatalog: {}\n".format(ps_cat_sw[i_obs]),
-                "      GalaxyCatalog: {}\n".format(GalaxyCatalog),
-                "      ExtendedCatalog: {}\n".format(ExtendedCatalog),
-                "      ExtendedScale: {}\n".format(ExtendedScale),
-                "      ExtendedCenter: {}\n".format(ExtendedCenter),
-                "      MovingTargetList: {}\n".format(MovingTargetList),
-                "      MovingTargetSersic: {}\n".format(MovingTargetSersic),
-                "      MovingTargetExtended: {}\n".format(MovingTargetExtended),
-                "      MovingTargetConvolveExtended: {}\n".format(MovingTargetConvolveExtended),
-                "      MovingTargetToTrack: {}\n".format(MovingTargetToTrack),
-                "      BackgroundRate: {}\n".format(BackgroundRate_sw),
-                "    LW:\n",
-                "      Filter: {}\n".format(lw_filt),
-                "      PointSourceCatalog: {}\n".format(ps_cat_lw[i_obs]),
-                "      GalaxyCatalog: {}\n".format(GalaxyCatalog),
-                "      ExtendedCatalog: {}\n".format(ExtendedCatalog),
-                "      ExtendedScale: {}\n".format(ExtendedScale),
-                "      ExtendedCenter: {}\n".format(ExtendedCenter),
-                "      MovingTargetList: {}\n".format(MovingTargetList),
-                "      MovingTargetSersic: {}\n".format(MovingTargetSersic),
-                "      MovingTargetExtended: {}\n".format(MovingTargetExtended),
-                "      MovingTargetConvolveExtended: {}\n".format(MovingTargetConvolveExtended),
-                "      MovingTargetToTrack: {}\n".format(MovingTargetToTrack),
-                "      BackgroundRate: {}\n\n".format(BackgroundRate_lw)
+                "Observation{}:\n".format(obs_number),
+                "  Name: '{}'\n".format(obs_names[i_obs]),
+                "  Date: {}\n".format(date),
+                "  PAV3: {}\n".format(PAV3),
             ]
+
+            for i_filt, (sw_filt, lw_filt) in enumerate(zip(sw_filters[obs_number],
+                                                            lw_filters[obs_number])):
+                write += [
+                    "  FilterConfig{}:\n".format(i_filt + 1),
+                    "    SW:\n",
+                    "      Filter: {}\n".format(sw_filt),
+                    "      PointSourceCatalog: {}\n".format(ps_cat_sw[i_obs]),
+                    "      GalaxyCatalog: {}\n".format(GalaxyCatalog),
+                    "      ExtendedCatalog: {}\n".format(ExtendedCatalog),
+                    "      ExtendedScale: {}\n".format(ExtendedScale),
+                    "      ExtendedCenter: {}\n".format(ExtendedCenter),
+                    "      MovingTargetList: {}\n".format(MovingTargetList),
+                    "      MovingTargetSersic: {}\n".format(MovingTargetSersic),
+                    "      MovingTargetExtended: {}\n".format(MovingTargetExtended),
+                    "      MovingTargetConvolveExtended: {}\n".format(MovingTargetConvolveExtended),
+                    "      MovingTargetToTrack: {}\n".format(MovingTargetToTrack),
+                    "      BackgroundRate: {}\n".format(BackgroundRate_sw),
+                    "    LW:\n",
+                    "      Filter: {}\n".format(lw_filt),
+                    "      PointSourceCatalog: {}\n".format(ps_cat_lw[i_obs]),
+                    "      GalaxyCatalog: {}\n".format(GalaxyCatalog),
+                    "      ExtendedCatalog: {}\n".format(ExtendedCatalog),
+                    "      ExtendedScale: {}\n".format(ExtendedScale),
+                    "      ExtendedCenter: {}\n".format(ExtendedCenter),
+                    "      MovingTargetList: {}\n".format(MovingTargetList),
+                    "      MovingTargetSersic: {}\n".format(MovingTargetSersic),
+                    "      MovingTargetExtended: {}\n".format(MovingTargetExtended),
+                    "      MovingTargetConvolveExtended: {}\n".format(MovingTargetConvolveExtended),
+                    "      MovingTargetToTrack: {}\n".format(MovingTargetToTrack),
+                    "      BackgroundRate: {}\n\n".format(BackgroundRate_lw)
+                ]
+
+    elif used_instruments[0] in ['NIRISS', 'FGS']:
+        for i_obs in range(num_obs):
+            obs_number = i_observations[i_obs] + 1
+            write += [
+                "Observation{}:\n".format(obs_number),
+                "  Name: '{}'\n".format(obs_names[i_obs]),
+                "  Date: {}\n".format(date),
+                "  PAV3: {}\n".format(PAV3),
+            ]
+
+            # for i_filt, filt in enumerate(filters[obs_number]):
+            for i_filt, filt in enumerate(filters[i_obs]):
+                write += [
+                    # "  FilterConfig{}:\n".format(i_filt + 1),
+                    # "    SW:\n",
+                    "  Filter: {}\n".format(filt),
+                    "  PointSourceCatalog: {}\n".format(catalog_files[i_obs]),
+                    "  GalaxyCatalog: {}\n".format(GalaxyCatalog),
+                    "  ExtendedCatalog: {}\n".format(ExtendedCatalog),
+                    "  ExtendedScale: {}\n".format(ExtendedScale),
+                    "  ExtendedCenter: {}\n".format(ExtendedCenter),
+                    "  MovingTargetList: {}\n".format(MovingTargetList),
+                    "  MovingTargetSersic: {}\n".format(MovingTargetSersic),
+                    "  MovingTargetExtended: {}\n".format(MovingTargetExtended),
+                    "  MovingTargetConvolveExtended: {}\n".format(MovingTargetConvolveExtended),
+                    "  MovingTargetToTrack: {}\n".format(MovingTargetToTrack),
+                    "  BackgroundRate: {}\n".format(BackgroundRate),
+                ]
 
     f = open(yaml_file, 'w')
     for line in write:
