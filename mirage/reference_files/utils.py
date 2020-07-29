34709f0378fdcfe010299bb4f151ef4011c3df7e
Bryan Hilbert <hilbert@stsci.edu>
2020-07-28 21:46:59 -0400

    Update trans file search assuming single file for imaging modes

--- mirage/reference_files/utils.py
+++ mirage/reference_files/utils.py
@@ -25,9 +25,7 @@ import os
 def get_transmission_file(parameter_dict):
     """Determine the best transmission image file to use for the given
     instrument/detector setup. This image will be located in either the
-    GRISM_NIRCAM or GRISM_NIRISS repositories. For FGS, return a transmission
-    image that is the size of the full frame detector and populated with 1.0
-    for all pixels.
+    GRISM_NIRCAM or GRISM_NIRISS repositories.
 
     Parameters
     ----------
@@ -37,37 +35,75 @@ def get_transmission_file(parameter_dict):
 
     Returns
     -------
-    filename : str
+    transmission_filename : str
         Full path to the transmission file
     """
+    datadir = os.environ.get('MIRAGE_DATA')
+
     if parameter_dict['INSTRUME'].lower() == 'nircam':
-        repo = 'GRISM_NIRCAM'
+        dirname = os.path.join(datadir, parameter_dict['INSTRUME'].lower(), 'GRISM_NIRCAM')
+
         # Assume that detector names in the transmission file names will
         # use 'NRCA5' rather than 'NRCALONG'
         if 'LONG' in parameter_dict['DETECTOR']:
             parameter_dict['DETECTOR'] = parameter_dict['DETECTOR'].replace('LONG', '5')
+
+        if parameter_dict['DETECTOR'] not in ['NRCA5', 'NRCB5', 'NRCALONG', 'NRCBLONG']:
+            # For NIRCam SW, we use the same file for all detectors/filters/pupils
+            transmission_filename = os.path.join(dirname, 'NIRCam_SW_transmission_image.fits')
+        elif parameter_dict['PUPIL'] not in ['GRISMR', 'GRISMC']:
+            # For LW imaging, we use the same file for all detectors/filters/pupils
+            transmission_filename = os.path.join(dirname, 'NIRCam_LW_imaging_transmission_image.fits')
+        else:
+            transmission_filename = search_transmission_files(dirname, parameter_dict)
+
+    # For NIRISS we search for a detector/filter/pupil-dependent file
     elif parameter_dict['INSTRUME'].lower() == 'niriss':
-        repo = 'GRISM_NIRISS'
+        dirname = os.path.join(datadir, parameter_dict['INSTRUME'].lower(), 'GRISM_NIRISS')
+        transmission_filename = search_transmission_files(dirname, parameter_dict)
+
     elif parameter_dict['INSTRUME'].lower() == 'fgs':
-        return 'create_in_place.fits'
+        dirname = os.path.join(datadir, 'niriss', 'GRISM_NIRISS')
+        transmission_filename = os.path.join(dirname, 'FGS_transmission_image.fits')
 
-    datadir = os.environ.get('MIRAGE_DATA')
-    dirname = os.path.join(datadir, parameter_dict['INSTRUME'].lower(), repo)
-    transmission_files = glob(os.path.join(dirname, '*transmission*fits'))
+    return transmission_file
 
-    filt = parameter_dict['FILTER']
-    pupil = parameter_dict['PUPIL']
-    detector = parameter_dict['DETECTOR']
+
+def search_transmission_files(directory, parameters):
+    """Find the transmission file within the collection of transmission files
+    that matches the provided detector, filter and pupil values
+
+    Parameters
+    ----------
+    directory : str
+        Name of directory containing the transmission file collection
+
+    parameter_dict : dict
+        Dictionary of basic metadata from the file to be processed by the
+        returned reference files (e.g. INSTRUME, DETECTOR, etc)
+
+    Returns
+    -------
+    match[0] : str
+        Full path to the transmission file
+    """
+    files = glob(os.path.join(directory, '*transmission*fits'))
+
+    filt = parameters['FILTER']
+    pupil = parameters['PUPIL']
+    detector = parameters['DETECTOR']
 
     # Search using filenames
-    if parameter_dict['INSTRUME'].lower() == 'nircam':
-        match = [file for file in transmission_files if ((filt in file) and (pupil in file) and (detector in file))]
-    elif parameter_dict['INSTRUME'].lower() == 'niriss':
-        match = [file for file in transmission_files if ((filt in file) and (pupil in file))]
+    if parameters['INSTRUME'].lower() == 'nircam':
+        match = [file for file in files if ((filt in file) and (pupil in file) and (detector in file))]
+    elif parameters['INSTRUME'].lower() == 'niriss':
+        match = [file for file in files if ((filt in file) and (pupil in file))]
+    else:
+        raise ValueError('Only NIRCam and NIRISS are supported in search_transmission_files()')
 
     if len(match) == 1:
         return match[0]
     elif len(match) == 0:
-        raise ValueError("No matching transmission image files found: {}".format(parameter_dict))
+        raise ValueError("No matching transmission image files found: {}".format(parameters))
     elif len(match) > 1:
         raise ValueError("More than one matching transmission image file found: {}".format(match))
