3be558c32b97311f49c7c492e5e558c50db33575
Bryan Hilbert <hilbert@stsci.edu>
2022-12-05 10:47:46 -0500

    Check for IndexError due to new pointing Glossary

--- mirage/apt/apt_inputs.py
+++ mirage/apt/apt_inputs.py
@@ -865,8 +865,6 @@ class AptInput:
 
                         # The exception to this rule is TA images.
                         # These have Exp values of 0. Sigh.
-
-
                         if ((int(elements[1]) > 0) & ('NRC' in elements[4]
                                                          or 'NIS' in elements[4]
                                                          or 'FGS' in elements[4]
@@ -934,7 +932,7 @@ class AptInput:
                             observation_id.append("V{}P{}{}{}{}".format(vid, '00000000', vgrp, seq, act))
                             # act_counter += 1
 
-                    except ValueError as e:
+                    except (ValueError, IndexError) as e:
                         if verbose:
                             self.logger.info('Skipping line:\n{}\nproducing error:\n{}'.format(line, e))
                         pass
